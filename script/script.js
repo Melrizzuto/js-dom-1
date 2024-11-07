2f26b46765a114e1901a3c42b5028c5290f89489
Melrizzuto <melrizzuto4@gmail.com>
2024-11-07 15:17:00 +0100

    added riferimenti js

--- script/script.js
+++ script/script.js
@@ -1,2 +1,8 @@
 "use script"
 console.clear();
+
+// Riferimento agli elementi dell'HTML
+const lampadina = document.getElementById('lampadina');
+const bottoneAccensione = document.getElementById('bottoneAccensione');
+console.log(lampadina);
+console.log(bottoneAccensione);
