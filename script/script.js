95b81e3bded68b9e9349c1a6edd5a2e85c0487cf
Melrizzuto <melrizzuto4@gmail.com>
2024-11-07 15:29:57 +0100

    added function button in/out light js

--- script/script.js
+++ script/script.js
@@ -2,7 +2,20 @@
 console.clear();
 
 // Riferimento agli elementi dell'HTML
-const lampadina = document.getElementById('lampadina');
+const lampadina = document.getElementById('lampadinaImg'); // Cambia qui per puntare all'immagine
 const bottoneAccensione = document.getElementById('bottoneAccensione');
 console.log(lampadina);
 console.log(bottoneAccensione);
+
+// Funzione per gestire accensione e spegnimento
+bottoneAccensione.addEventListener('click', function() {
+    if (bottoneAccensione.textContent.includes("Accendi")) {
+        // Accendi la lampadina
+        lampadina.src = "../img/yellow_lamp.png"; // Cambia con l'immagine accesa
+        bottoneAccensione.textContent = "Spegni";
+    } else {
+        // Spegni la lampadina
+        lampadina.src = "../img/white_lamp.png"; // Cambia con l'immagine spenta
+        bottoneAccensione.textContent = "Accendi";
+    }
+});
